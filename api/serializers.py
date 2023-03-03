387cf570d33218e4d275bed2eca1135b78077aeb
Сергей Смирнов <sm-serega85@yandex.ru>
2023-03-03 16:33:13 +0300

    sprint8_20230303

--- api/serializers.py
+++ api/serializers.py
@@ -20,9 +20,14 @@ class GroupSerializer(serializers.ModelSerializer):
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(read_only=True,
-                                          slug_field='username')
-    group = serializers.SlugRelatedField(read_only=True, slug_field='title')
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+    group = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='title'
+    )
 
     class Meta:
         model = Post
